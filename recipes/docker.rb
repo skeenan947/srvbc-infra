6b0ebdaebb3faff8a520af9dc51b0c3b3964599b
Shaun Keenan <shaun_keenan@gap.com>
2015-08-06 11:40:20 -0700

    oops, let's write docker code to the docker recipe

---
+++ recipes/docker.rb
@@ -0,0 +1,30 @@
+#
+# Cookbook Name:: srvbc-infra
+# Recipe:: docker 
+#
+# Copyright (c) 2015 Shaun Keenan, All Rights Reserved.
+require 'chef/provisioning/docker_driver'
+
+node['provisioning']['hosts'].each do |nodetype,h|
+  machine_image nodetype do
+    role 'base'
+    role nodetype
+    machine_options :docker_options => {
+      :base_image => {
+        :name => 'ubuntu',
+        :repository => 'ubuntu',
+        :tag => '14.04'
+      }
+    }
+  end
+  
+  1.upto(h['count']) do |num|
+    machine "#{nodetype}-#{num}" do
+      from_image nodetype
+      machine_options :docker_options => {
+        :command => '/usr/sbin/sshd -p 8022 -D',
+        :ports => 8022
+      }
+    end
+  end
+end
--- recipes/vsphere.rb
+++ recipes/vsphere.rb
@@ -3,28 +3,44 @@
 # Recipe:: vsphere 
 #
 # Copyright (c) 2015 Shaun Keenan, All Rights Reserved.
-require 'chef/provisioning/docker_driver'
+require 'chef/provisioning/vsphere_driver'
+
+chef_gem 'chef-provisioning-vsphere' do
+  action :install
+  compile_time true
+end
+
+p = node['provisioning']
+creds = data_bag_item('provisioning_creds','vsphere')
+
+with_vsphere_driver host: p['vsphere_host'],
+  insecure: true,
+  user:     creds['vsphere_user'],
+  password: creds['vsphere_pass']
+
+
+with_machine_options :bootstrap_options => {
+  use_linked_clone: false,
+  network_name: [p['network']],
+  datacenter: p['datacenter_name'],
+  template_name: p['template'],
+  :ssh => {
+    :paranoid => false,
+  }
+}
+
 
 node['provisioning']['hosts'].each do |nodetype,h|
-  machine_image nodetype do
-    role 'base'
-    role nodetype
-    machine_options :docker_options => {
-      :base_image => {
-        :name => 'ubuntu',
-        :repository => 'ubuntu',
-        :tag => '14.04'
-      }
-    }
-  end
-  
   1.upto(h['count']) do |num|
     machine "#{nodetype}-#{num}" do
-      from_image nodetype
-      machine_options :docker_options => {
-        :command => '/usr/sbin/sshd -p 8022 -D',
-        :ports => 8022
+      num_cpus: h['num_cpus'],
+      memory_mb: h['memory_mb'],
+      template_name: p['templates'][h['template']]['name']
+      ssh => {
+        :user => p['templates'][h['template']]['ssh_user']
       }
+      role 'base',
+      role nodetype
     end
   end
 end
